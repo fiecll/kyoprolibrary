5559235f8fe9d5cc055ef5f79e798413d2bf8894
fiecll <fiecll@users.noreply.github.com>
2024-11-07 15:07:39 +0000

    deploy: 8ba9c45253b86696d8b20c239762f6ec917e3e60

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,12 +1,14 @@
 function copyCode(button) {
-    const codeBlock = button.previousElementSibling.textContent;
-    navigator.clipboard.writeText(codeBlock).then(() => {
-      button.textContent = "Copied!";
-      setTimeout(() => {
-        button.textContent = "Copy";
-      }, 2000);
-    }).catch(err => {
-      console.error("Failed to copy: ", err);
-    });
+    const codeBlock = button.previousElementSibling.textContent.trim();
+    navigator.clipboard.writeText(codeBlock)
+      .then(() => {
+        button.textContent = "Copied!";
+        setTimeout(() => {
+          button.textContent = "Copy";
+        }, 2000);
+      })
+      .catch(err => {
+        console.error("Failed to copy text: ", err);
+      });
   }
   
